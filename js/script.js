c371582a737802b3a535dc9316219ef0723258b7
Michael Williams <williams.michaela@outlook.com>
2020-05-03 16:43:30 -0600

    Final Project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+$(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+  
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
+    });
+  });
